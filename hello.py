622dcf9b41cbdf57f5383966e5e674477d660c6c
Ioannis Anastopoulos <ianastop@ucsc.edu>
2020-02-24 18:48:42 -0800

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-from os import path 
-
-print('hello')
-print ("model exist:"+str(path.exists('/app/RiboVsPoly.sav')))
-print ("model exist:"+str(path.exists('RiboVsPoly.sav')))
-print ("hello exists:" + str(path.exists('hello.py')))
-print ("RF exists:" + str(path.exists('RF_deploy.py')))
-print ("RF exists:" + str(path.exists('/app/RF_deploy.py')))
