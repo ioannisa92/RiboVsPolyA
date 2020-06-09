2b551a97320a593eeb6c000627f7e3589ee35582
ioannisa92 <ianastop@ucsc.edu>
2020-06-09 16:34:33 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -11,13 +11,15 @@ wget --quiet -O ./data_test/TreehousePEDv9_Ribodeplete_unique_hugo_log2_tpm_plus
 wget --quiet -O ./data_test/clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv https://xena.treehouse.gi.ucsc.edu/download/clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv
 wget --quiet -O ./data_test/TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv
 
-echo Creating Unbalanced dataset...
-#run script that selects random samples from PolyA to create MergedData_reduced.tsv, and MergedLabels_reduced.ts
-python ./scripts/merge_unbalanced.py
+# echo Creating Unbalanced dataset...
+# run script that selects random samples from PolyA to create MergedData_reduced.tsv, and MergedLabels_reduced.tsv
+# python ./scripts/merge_unbalanced.py
 
 echo Creating Balanced dataset...
-#run script that selects random samples from PolyA to create MergedData_reduced.tsv, and MergedLabels_reduced.ts
+#run script that selects random samples from PolyA to create MergedData_reduced.tsv, and MergedLabels_reduced.tsv
 python ./scripts/merge_balanced.py
+
+
 
 echo DONE
 # make notebooks under examples to take these files as input
