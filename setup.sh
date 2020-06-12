692f2ecdda04723a5664fdc14ae2719d4c41d35e
ioannisa92 <ianastop@ucsc.edu>
2020-06-12 23:40:49 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -38,15 +38,15 @@ wget --quiet -O ${DATADIR}TreehousePEDv9_Ribodeplete_unique_hugo_log2_tpm_plus_1
 wget --quiet -O ${DATADIR}clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv https://xena.treehouse.gi.ucsc.edu/download/clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv
 wget --quiet -O ${DATADIR}TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv
 
-echo Downloading openPBTA files...
-wget --quiet -O ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.stranded.rds
+#echo Downloading openPBTA files...
+#wget --quiet -O ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.stranded.rds
 
-wget --quiet -O ${DATADIR}pbta-gene-expression-rsem-tpm.polya.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.polya.rds
+#wget --quiet -O ${DATADIR}pbta-gene-expression-rsem-tpm.polya.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.polya.rds
 
-echo Converting openPBTA files to tsv...
-python ./scripts/rds_to_pandas.py -i ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds -o ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.tsv
+#echo Converting openPBTA files to tsv...
+#python ./scripts/rds_to_pandas.py -i ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds -o ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.tsv
 
-python ./scripts/rds_to_pandas.py -i ${DATADIR}pbta-gene-expression-rsem-tpm.polya.rds -o ${DATADIR}pbta-gene-expression-rsem-tpm.polya.tsv
+#python ./scripts/rds_to_pandas.py -i ${DATADIR}pbta-gene-expression-rsem-tpm.polya.rds -o ${DATADIR}pbta-gene-expression-rsem-tpm.polya.tsv
 
 echo Creating Balanced dataset...
 #run script that selects random samples from PolyA to create MergedData_reduced.tsv, and MergedLabels_reduced.tsv
