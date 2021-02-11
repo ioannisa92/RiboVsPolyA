38ea9d5956289015b88f7f4ed5b4b4d46403eaf5
ioannisa92 <ianastop@ucsc.edu>
2021-02-11 22:07:13 +0000

    data pp change

--- RF_deploy.sh
+++ RF_deploy.sh
@@ -1,15 +1,22 @@
 DATA=./SRP_data/refine_bio_SRP_data/
 TREEHOUSEDATA=./SRP_data/treehouse_SRP_data/
-MAXDEPTH=1
+MAXDEPTH=3
 
-python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP183700/SRP183700_log2TPM_plus1_HUGO.tsv -o ./results/SRP183700_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
+# python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP183700/SRP183700_log2TPM_plus1_HUGO.tsv -o ./results/SRP183700_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
 
-python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP109549/SRP109549_log2TPM_plus1_HUGO.tsv -o ./results/SRP109549_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
+# python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP109549/SRP109549_log2TPM_plus1_HUGO.tsv -o ./results/SRP109549_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
 
-python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP130971/SRP130971_log2TPM_plus1_HUGO.tsv -o ./results/SRP130971_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
+# python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP130971/SRP130971_log2TPM_plus1_HUGO.tsv -o ./results/SRP130971_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
 
-python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP058841/SRP058841_log2TPM_plus1_HUGO.tsv -o ./results/SRP058841_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
+# python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP058841/SRP058841_log2TPM_plus1_HUGO.tsv -o ./results/SRP058841_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
 
+# python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP064410/SRP064410_log2TPM_plus1_HUGO.tsv -o ./results/SRP064410_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
+
+# python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP026013/SRP026013_log2TPM_plus1_HUGO.tsv -o ./results/SRP026013_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
+
+python ./scripts/RF_deploy.py -i ./data/SRP127360_TPM_log2_plus_1.tsv -o ./results/SRP127360_TPM_log2_plus_1.TEST.balanced_w_unbalanced_params_results.tsv -model ./models/RiboVsPoly_balanced_w_unbalanced_params.sav
+
+python ./scripts/RF_deploy.py -i ${TREEHOUSEDATA}/SRP127360/SRP127360_log2TPM_plus1_HUGO.tsv -o ./results/SRP127360_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
 
 #python ./scripts/RF_deploy.py -i ${DATA}SRP055411_log2TPM_plus1_HUGO.tsv -o ./results/SRP055411_TPM_log2_plus_1.TH.balanced_maxdepth${MAXDEPTH}_results.tsv -model ./models/RiboVsPoly_balanced_max_depth_${MAXDEPTH}.sav
 
