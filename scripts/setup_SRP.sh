d2596ef1f2ccefa4733f8d3888bf5fc84d032827
geofflyle <geofflyle@gmail.com>
2022-02-24 22:40:02 +0000

    Show wget download progress

--- scripts/setup_SRP.sh
+++ scripts/setup_SRP.sh
@@ -19,39 +19,39 @@ fi
 # Download SRP data from figshare
 echo Downloading SRP data processed via Treehouse pipeline
 
-wget -O ${TREEHOUSEDATA}SRP026013_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193727
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP026013_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193727
 
-wget -O ${TREEHOUSEDATA}SRP055411_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193730
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP055411_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193730
 
-wget -O ${TREEHOUSEDATA}SRP058841_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193733
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP058841_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193733
 
-wget -O ${TREEHOUSEDATA}SRP064410_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193736
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP064410_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193736
 
-wget -O ${TREEHOUSEDATA}SRP109549_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193739
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP109549_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193739
 
-wget -O ${TREEHOUSEDATA}SRP127360_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193742
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP127360_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193742
 
-wget -O ${TREEHOUSEDATA}SRP130971_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193745
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP130971_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193745
 
-wget -O ${TREEHOUSEDATA}SRP132968_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193748
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP132968_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193748
 
-wget -O ${TREEHOUSEDATA}SRP183700_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193751
+wget --quiet --show-progress -O ${TREEHOUSEDATA}SRP183700_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193751
 
 echo Downloading SRP data processed via refine.bio pipeline
 
-wget -O ${REFINEBIODATA}SRP026013_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193775
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP026013_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193775
 
-wget -O ${REFINEBIODATA}SRP055411_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193781
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP055411_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193781
 
-wget -O ${REFINEBIODATA}SRP058841_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193784
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP058841_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193784
 
-wget -O ${REFINEBIODATA}SRP064410_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193790
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP064410_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193790
 
-wget -O ${REFINEBIODATA}SRP109549_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193799
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP109549_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193799
 
-wget -O ${REFINEBIODATA}SRP130971_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193802
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP130971_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193802
 
-wget -O ${REFINEBIODATA}SRP132968_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193805
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP132968_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193805
 
-wget -O ${REFINEBIODATA}SRP183700_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193808
+wget --quiet --show-progress -O ${REFINEBIODATA}SRP183700_log2TPM_plus1_HUGO.tsv https://ndownloader.figshare.com/files/27193808
 
--- setup.sh
+++ setup.sh
@@ -41,18 +41,18 @@ python -m pip install --no-cache-dir -r requirements.txt
 
 echo Downloading Expression and Clinical PolyA and RiboD files...
 # Download Treehouse PolyA, RiboD expression files
-wget --quiet -O ${DATADIR}TumorCompendium_v10_PolyA_hugo_log2tpm_58581genes_2019-07-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TumorCompendium_v10_PolyA_hugo_log2tpm_58581genes_2019-07-25.tsv
+wget --quiet --show-progress -O ${DATADIR}TumorCompendium_v10_PolyA_hugo_log2tpm_58581genes_2019-07-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TumorCompendium_v10_PolyA_hugo_log2tpm_58581genes_2019-07-25.tsv
 
-wget --quiet -O ${DATADIR}TreehousePEDv9_Ribodeplete_unique_hugo_log2_tpm_plus_1.2019-03-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TreehousePEDv9_Ribodeplete_unique_hugo_log2_tpm_plus_1.2019-03-25.tsv
+wget --quiet --show-progress -O ${DATADIR}TreehousePEDv9_Ribodeplete_unique_hugo_log2_tpm_plus_1.2019-03-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TreehousePEDv9_Ribodeplete_unique_hugo_log2_tpm_plus_1.2019-03-25.tsv
 
 # Download PolyA, RiboD clinical files
-wget --quiet -O ${DATADIR}clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv https://xena.treehouse.gi.ucsc.edu/download/clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv
-wget --quiet -O ${DATADIR}TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv
+wget --quiet --show-progress -O ${DATADIR}clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv https://xena.treehouse.gi.ucsc.edu/download/clinical_TumorCompendium_v10_PolyA_2019-07-25.tsv
+wget --quiet --show-progress -O ${DATADIR}TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv https://xena.treehouse.gi.ucsc.edu/download/TreehousePEDv9_Ribodeplete_clinical_metadata.2019-03-25.tsv
 
 echo Downloading openPBTA files...
-wget --quiet -O ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.stranded.rds
+wget --quiet --show-progress -O ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.stranded.rds
 
-wget --quiet -O ${DATADIR}pbta-gene-expression-rsem-tpm.polya.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.polya.rds
+wget --quiet --show-progress -O ${DATADIR}pbta-gene-expression-rsem-tpm.polya.rds  https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data/release-v16-20200320/pbta-gene-expression-rsem-tpm.polya.rds
 
 echo Converting openPBTA files to tsv...
 python ./scripts/rds_to_pandas.py -i ${DATADIR}pbta-gene-expression-rsem-tpm.stranded.rds -o ${DATADIR}pbta-gene-expression-rsem-log2tpm_plus_1.stranded.tsv
